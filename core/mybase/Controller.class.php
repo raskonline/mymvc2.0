174519078a76ce0860e435692b119bebcb25de28
rask <2083967667@qq.com>
2018-07-06 17:33:13 +0800

    添加了smarty引擎模板

--- core/mybase/Controller.class.php
+++ core/mybase/Controller.class.php
@@ -1,33 +1,59 @@
 <?php
+
 namespace core\mybase;
+
 use core\Application;
 
 /**控制器基类*/
 class Controller
 {
     protected $view;//视图对象
+    protected $smarty;//模板引擎
 
     /**子类对象如果没有定义 __construct()，子类在实例化的时候会自动调用父类 __construct()。*/
     public function __construct()
     {
         $this->initView();//初始化视图
+        $this->initSmarty();//初始化smarty
     }
 
     /**初始化视图*/
-    private function initView(){
+    private function initView()
+    {
         //初始化视图对象
-        $this->view=new View();
+        $this->view = new View();
         // 视图模板的路径： application/home/views/index/index.html
-        $this->view->view_dir="application/".Application::$group."/views/".strtolower(Application::$controller)."/";
+        $this->view->view_dir = "application/" . Application::$group . "/views/" . strtolower(Application::$controller) . "/";
     }
 
     /**调用视图对象呈现视图方法*/
-    protected function display($viewname=null){
-        $this->view->display($viewname);
+    protected function display($view_name = null)
+    {
+        $view_name = isset($view_name) ? $view_name : Application::$action;//视图名称默认为方法名称
+        $view_name = $view_name . ".html";
+        //$this->view->display($view_name);
+        $this->smarty->display($view_name);
     }
 
     /**调用视图对象数据填充方法*/
-    protected function assign($name,$value){
-        $this->view->assign($name,$value);
+    protected function assign($name, $value)
+    {
+        //$this->view->assign($name, $value);
+        $this->smarty->assign($name, $value);
+    }
+
+    /**初始化smarty*/
+    private function initSmarty()
+    {
+        //初始化smarty
+        $this->smarty = new \Smarty();
+        //设置模板路径
+        $this->smarty->setTemplateDir("application/" . Application::$group . "/views/" . strtolower(Application::$controller) . "/");
+        //设置混编文件路径
+        $this->smarty->setCompileDir("runtime/" . Application::$group . "/" . strtolower(Application::$controller) . "/");
+        //设置分隔符
+        $this->smarty->setLeftDelimiter("<{");
+        $this->smarty->setRightDelimiter("}>");
+        //或者把js或者css代码放在{literal}{/literal}之间
     }
 }
--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
 //入口文件
 //1.加载配置文件和常量
-require "configs/config.php";
-require "configs/constants.php";
+require_once "configs/config.php";
+require_once "configs/constants.php";
 
-require 'core/PHPMailer/class.phpmailer.php';
-require 'core/PHPMailer/class.smtp.php';
+require_once 'core/PHPMailer/class.phpmailer.php';
+require_once 'core/PHPMailer/class.smtp.php';
 
-//2.注册自动加载方法
+//2.引入smarty
+require_once 'core/smarty/Smarty.class.php';
+
+//3.注册自动加载方法
 require "core/MyLoad.class.php";
 \core\MyLoad::registerAutoLoad();
 
-//3.启动核心运行类(前端处理器)
+//4.启动核心运行类(前端处理器)
 \core\Application::run();
 
 
